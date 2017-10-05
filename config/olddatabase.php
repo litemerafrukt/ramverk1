c4ed06a64288ca48a8ee7dd39640ff1392879aeb
Anders Nygren <litemerafrukt@gmail.com>
2017-10-05 08:40:47 +0200

    secrets

--- config/olddatabase.php
+++ config/olddatabase.php
@@ -3,11 +3,11 @@
 // mysql -hblu-ray.student.bth.se -uanng15 -p
 
 return [
-    // $dsn      = "mysql:host=blu-ray.student.bth.se;dbname=anng15;",
-    // $login    = "anng15",
-    // $password = "xrVazvnZQDNb",
-    $dsn      = "mysql:host=localhost;dbname=anng15;",
-    $login    = "whoever",
-    $password = "whatever",
-    $options  = [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"],
+    $dsn = "mysql:host=blu-ray.student.bth.se;dbname=anng15;",
+    $login = "anng15",
+    $password = "xrVazvnZQDNb",
+    // $dsn      = "mysql:host=localhost;dbname=anng15;",
+    // $login    = "whoever",
+    // $password = "whatever",
+    $options = [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"],
 ];
